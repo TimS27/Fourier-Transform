e4658e641d8545361a7b7b388ad6d7b31012c2fb
Tim Schoenwald <timschoenwald@googlemail.com>
2024-10-04 16:17:23 +0200

    implemeted Furioso for spectral data

--- ft.py
+++ ft.py
@@ -1,52 +1,62 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.constants
+import scipy.signal
+import obspy.signal.filter
 import fourioso
 
 # reading CSV file
 data = pd.read_csv("Spectrum-5mm-Fused-Silica.csv")
  
-# converting column data to list
-wavelength = np.array(data['wavelength'].tolist())
+# converting column data to list then array
+wavelengths = np.array(data['wavelength'].tolist())
 counts = np.array(data['counts'].tolist())
+
+# covert wavelengths to frequencies
+frequencies = scipy.constants.c / (wavelengths * 1e-9)
+# intensity to field
+field = np.sqrt(counts)
  
-# printing list data
-#print('Wavelegths:', wavelength)
-#print('Counts:', counts)
-#print(type(np.array(wavelength)))
+nu_span = frequencies[0] - frequencies[len(frequencies)-1]
+max_nuspacing = np.abs(np.diff(frequencies).max())
 
-# Interpolate
+n_points, nu_spacing = fourioso.n_spacing(max_nuspacing, nu_span) # automatically chooses efficient n_points
+nu = fourioso.get_axis(n_points, nu_spacing)
 
-x = np.linspace(736.24261 ,1321.4418, 10000)
-xp = wavelength
-fp = counts
-spectrum_interpolated = np.interp(x, xp, fp)
+# Interpolate spectral data to new frequency axis
+spectrum_interpolated = np.interp(nu, frequencies, field)
 
-#plt.plot(x, spectrum_interpolated)
-#plt.ylabel('counts')
-#plt.show()
-
-#plt.plot(wavelength, counts)
-#plt.show()
-
-t_span = 50
-max_tspacing = 0.1
-
-#n_points, t_spacing = fourioso.n_spacing(max_tspacing, t_span) # automatically chooses efficient n_points
-#t = fourioso.get_axis(n_points, t_spacing)
-
-#data = np.exp(-t**2)
-
-nu, data_transformed = fourioso.transform(wavelength, counts)
+t, data_transformed = fourioso.itransform(nu, spectrum_interpolated)
 # you can also separate this:
 #   nu = fourioso.transform(t)
 #   data_transformed = fourioso.transform(t, data, return_axis=False)
 
-data_backtransformed = fourioso.itransform(nu, data_transformed, return_axis=False)
+data_backtransformed = fourioso.transform(t, data_transformed, return_axis=False)
+
+intensity_envelope = obspy.signal.filter.envelope(data_transformed**2)
+
+# Determine FWHM in fs
+def get_fwhm(t, envelope):
+  It = abs(envelope)**2
+  spline = scipy.interpolate.UnivariateSpline(t, It-np.max(It)/2, s=0)
+
+  try:
+    r1, r2 = spline.roots()
+    return r2-r1
+  except ValueError:
+    return np.nan
+
+fwhm = get_fwhm(t, intensity_envelope) * 1e15
+
+print("FWHM: " + str(fwhm) + " fs")
 
 plt.figure()
-plt.plot(wavelength, data, 'x')
-plt.plot(wavelength, data_backtransformed, '+')
+plt.plot(t, intensity_envelope, '+')
 plt.show()
 
-#DOESNT WORK BECAUSE DATA HAS TO BE INTERPOLATED TO NEW AXIS
+
+
+
+
+
