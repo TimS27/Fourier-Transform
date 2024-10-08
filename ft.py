178087440ef4cf2304f19cfad3df8c5c50d97cc8
Tim Schoenwald <timschoenwald@googlemail.com>
2024-10-08 18:23:51 +0200

    Fixed Interpolation Problem -> Code works

--- ft.py
+++ ft.py
@@ -6,46 +6,43 @@ import scipy.signal
 import obspy.signal.filter
 import fourioso
 
-# reading CSV file
+# Reading CSV file
 data = pd.read_csv("Spectrum-5mm-Fused-Silica.csv")
  
-# converting column data to list then array
+# Converting column data to list then array
 wavelengths = np.array(data['wavelength'].tolist())
 counts = np.array(data['counts'].tolist())
 
-# covert wavelengths to frequencies
-frequencies = scipy.constants.c / (wavelengths * 1e-9)
-# intensity to field
+# Covert wavelengths to frequencies
+frequencies = scipy.constants.c / (wavelengths* 1e-9)
+# Intensity to field
 field = np.sqrt(counts)
 
+# Reverse frequencies and field because monotonically increasing xo is needed for interpolation with numpy.interp
 frequencies_reversed = frequencies[::-1]
 field_reversed = field[::-1]
 
+
 nu_span =  frequencies_reversed[len(frequencies_reversed)-1] - frequencies_reversed[0]
 max_nuspacing = np.abs(np.diff(frequencies_reversed).max())
-#print(nu_span)
-#print(nu_span/max_nuspacing)
-
-#nu_span = frequencies[0] - frequencies[len(frequencies)-1]
-#max_nuspacing = np.abs(np.diff(frequencies).max())
 
 n_points, nu_spacing = fourioso.n_spacing(max_nuspacing, nu_span) # automatically chooses efficient n_points
 nu = fourioso.get_axis(n_points, nu_spacing)
-#print(nu/1e12)
-#print(frequencies)
+
 
 # Test if arrax x is monotonic and can therefore be interpolated with np.interp
 def monotonic(x):
     dx = np.diff(x)
     return np.all(dx <= 0) or np.all(dx >= 0)
 
-#print(monotonic(frequencies_reversed))
+# Check if strictly increasing
+#print(np.all(np.diff(frequencies_reversed) > 0))
+
 
 # Interpolate spectral data to new frequency axis
-spectrum_interpolated = np.interp(nu, frequencies_reversed, field_reversed)
-#print(frequencies_reversed)
-print(type(field_reversed[1]))
-print(spectrum_interpolated[500])
+nu_interpolatable = nu + frequencies_reversed[0] - nu[0]  # get new frequency axis in data range for interpolation
+spectrum_interpolated = np.interp(nu_interpolatable, frequencies_reversed, field_reversed)
+
 
 t, data_transformed = fourioso.itransform(nu, spectrum_interpolated)
 # you can also separate this:
@@ -54,6 +51,7 @@ t, data_transformed = fourioso.itransform(nu, spectrum_interpolated)
 
 data_backtransformed = fourioso.transform(t, data_transformed, return_axis=False)
 
+# Optionally calculate envelope via Hilbert transform
 intensity_envelope = obspy.signal.filter.envelope(data_transformed**2)
 
 # Determine FWHM in fs
@@ -69,11 +67,15 @@ def get_fwhm(t, envelope):
 
 fwhm = get_fwhm(t, data_transformed) * 1e15
 
-#print("FWHM: " + str(fwhm) + " fs")
+print("FWHM: " + str(fwhm) + " fs")
 
 plt.figure()
-plt.plot(t, data_transformed, '+')
-#plt.show()
+plt.plot(t, data_transformed**2, '-o', markersize=2)
+plt.xlim(-1e-12, 1e-12)
+plt.title('Time domain intensity')
+plt.xlabel('Time [s]')
+plt.ylabel('Intensity')
+plt.show()
 
 
 
