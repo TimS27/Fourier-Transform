cd81541faba4753e9a956985f0ce78b695b354e1
Tim Schoenwald <timschoenwald@googlemail.com>
2024-10-09 14:59:09 +0200

    Plotting results in one figure, fixes, cleanup

--- ft.py
+++ ft.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.constants
 import scipy.signal
-import obspy.signal.filter
 import fourioso
+#import obspy.signal.filter
 
 # Reading CSV file
 data = pd.read_csv("Spectrum-5mm-Fused-Silica.csv")
@@ -27,13 +27,13 @@ nu_span =  frequencies_reversed[len(frequencies_reversed)-1] - frequencies_rever
 max_nuspacing = np.abs(np.diff(frequencies_reversed).max())
 
 n_points, nu_spacing = fourioso.n_spacing(max_nuspacing, nu_span) # automatically chooses efficient n_points
-nu = fourioso.get_axis(n_points, nu_spacing)
-
+nu = fourioso.get_axis(n_points, nu_spacing) # frequencies centered around 0
+nu_THz = nu / 1e12  # calculate new frequency axis in THz
 
 # Test if arrax x is monotonic and can therefore be interpolated with np.interp
-def monotonic(x):
-    dx = np.diff(x)
-    return np.all(dx <= 0) or np.all(dx >= 0)
+#def monotonic(x):
+#    dx = np.diff(x)
+#    return np.all(dx <= 0) or np.all(dx >= 0)
 
 # Check if strictly increasing
 #print(np.all(np.diff(frequencies_reversed) > 0))
@@ -48,11 +48,13 @@ t, data_transformed = fourioso.itransform(nu, spectrum_interpolated)
 # you can also separate this:
 #   nu = fourioso.transform(t)
 #   data_transformed = fourioso.transform(t, data, return_axis=False)
+t_fs = t * 1e15 # calculate time axis in fs
 
 data_backtransformed = fourioso.transform(t, data_transformed, return_axis=False)
 
 # Optionally calculate envelope via Hilbert transform
-intensity_envelope = obspy.signal.filter.envelope(data_transformed**2)
+#intensity_envelope = obspy.signal.filter.envelope(data_transformed**2)
+
 
 # Determine FWHM in fs
 def get_fwhm(t, envelope):
@@ -66,19 +68,41 @@ def get_fwhm(t, envelope):
     return np.nan
 
 fwhm = get_fwhm(t, data_transformed) * 1e15
-
 print("FWHM: " + str(fwhm) + " fs")
 
-plt.figure()
-plt.plot(t, data_transformed**2, '-o', markersize=2)
-plt.xlim(-1e-12, 1e-12)
-plt.title('Time domain intensity')
-plt.xlabel('Time [s]')
-plt.ylabel('Intensity')
+
+# Plot spectrum, electric field, and intensity in one figure
+fig, axs = plt.subplots(3, figsize=(8,14), gridspec_kw={'hspace': 0.5, 'wspace': 0.2})
+fig.suptitle('Spectrum, Electric Field, and Intensity via Fourier Transform')
+
+axs[0].plot(nu_THz, spectrum_interpolated, 'o', markersize=2)
+axs[0].set_title('Spectrum')
+axs[0].set_xlabel('Frequency span [THz]') # frequencies
+axs[0].set_ylabel('Spectrum [counts]')
+
+axs[1].plot(t_fs, data_transformed, '-o', markersize=2)
+axs[1].set_xlim(-1000, 1000)  # restrict x axis to puls 
+axs[1].set_title('Time domain electric field')
+axs[1].set_xlabel('Time [fs]')
+axs[1].set_ylabel('Electric field')
+fwhm_text = "FWHM: " + str(round(fwhm, 2)) + " fs"
+axs[1].text(250, -0.5e16, fwhm_text, fontsize=12)
+
+axs[2].plot(t_fs, data_transformed**2, '-o', markersize=2)
+axs[2].set_xlim(-1000, 1000)
+axs[2].set_title('Time domain intensity')
+axs[2].set_xlabel('Time [fs]')
+axs[2].set_ylabel('Intensity')
+
 plt.show()
 
-
-
-
-
-
+# Simple plotting option
+'''
+plt.figure()
+plt.plot(t, data_transformed, '-o', markersize=2)
+plt.xlim(-1e-12, 1e-12)
+plt.title('Time domain electric field')
+plt.xlabel('Time [s]')
+plt.ylabel('Electric field')
+plt.show()
+'''
